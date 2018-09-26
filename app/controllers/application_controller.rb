042d3951ac3805687d3dd21a9bf4e65f446e6b70
Tao Liu <taoliudesign@gmail.com>
2018-09-26 07:24:46 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < Sinatra::Base
   
   patch '/posts/:id' do
     @post = Post.find params[:id]
-    @post.update {name: params[:name], content: params[:content]}
+    @post.update name: params[:name], content: params[:content]
   end
   
   delete '/posts/:id' do
